25ff73d5739136f8a44b5b1d311d64a754b8392f
Vishok Sharma <vishokmait@gmail.com>
2026-03-05 11:48:31 +0530

    build output for metric types

---
+++ packages/shared-types/src/metric-type.d.ts
@@ -0,0 +1,4 @@
+export declare enum MetricType {
+    REQUEST = "request",
+    SYSTEM = "system"
+}
---
+++ packages/shared-types/src/metric-type.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.MetricType = void 0;
+var MetricType;
+(function (MetricType) {
+    MetricType["REQUEST"] = "request";
+    MetricType["SYSTEM"] = "system";
+})(MetricType || (exports.MetricType = MetricType = {}));
