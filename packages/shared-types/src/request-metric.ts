bcef46d693eb31a9ec5988084145ab5f7f198acf
Vishok Sharma <vishokmait@gmail.com>
2026-03-06 20:41:06 +0530

    add request metric schema

---
+++ packages/shared-types/src/request-metric.ts
@@ -0,0 +1,13 @@
+import { BaseMetric } from "./metric-base";
+import { MetricType } from "./metric-type";
+
+export interface RequestMetric extends BaseMetric {
+  type: MetricType.REQUEST;
+
+  method: string;        
+  route: string;         
+  statusCode: number;    
+
+  latencyMs: number;     
+  isError: boolean;     
+}
