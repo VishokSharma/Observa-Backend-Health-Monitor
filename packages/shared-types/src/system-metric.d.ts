0568ca5efbb5523babca023f3072e95413bb78d4
Vishok Sharma <vishokmait@gmail.com>
2026-03-09 13:35:54 +0530

    dist files for system metrics

---
+++ packages/shared-types/src/system-metric.d.ts
@@ -0,0 +1,7 @@
+import { BaseMetric } from "./metric-base";
+import { MetricType } from "./metric-type";
+export interface SystemMetric extends BaseMetric {
+    type: MetricType.SYSTEM;
+    cpuUsagePercent: number;
+    memoryUsageMb: number;
+}
